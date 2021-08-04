f0084aaa04a579df2f040eca01bdc817e42a489d
ashleynexvelsolutions <52248671+ashleynexvelsolutions@users.noreply.github.com>
2021-08-04 15:08:57 -0400

    ran build command

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,3 @@ import ReactWeather from './components/ReactWeather';
 
 export default ReactWeather;
 export { default as useOpenWeather } from './providers/openweather/useOpenWeather';
-export { default as useWeatherBit } from './providers/weatherbit/useWeatherBit';
